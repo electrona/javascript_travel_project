8a1a323e15912135048a47c1ed959c4d625e955a
Eric Thomas <ericthomas@tuta.io>
2021-11-30 18:27:15 -0330

    starter code and asset aquirement

---
+++ travel_agency.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const $ = (selector) => document.querySelector(selector);
+
+// place constants(methods) here
+
+document.addEventListener("DOMContentLoaded", () => {
+//place code here
+});
