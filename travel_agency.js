37c563010c45bb2690f4ba025571afda857425e4
neilstrattoncodes <95373851+neilstrattoncodes@users.noreply.github.com>
2021-12-09 18:18:01 -0330

    added phase two buttons

--- travel_agency.js
+++ travel_agency.js
@@ -7,3 +7,20 @@ const $ = (selector) => document.querySelector(selector);
 document.addEventListener("DOMContentLoaded", () => {
 //place code here
 });
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
