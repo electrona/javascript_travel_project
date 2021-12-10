f76421508a8382a125f53e205129f64dfa425502
neilstrattoncodes <95373851+neilstrattoncodes@users.noreply.github.com>
2021-12-09 22:49:39 -0330

    fixed text button layout and added booking.html

--- travel_agency.js
+++ travel_agency.js
@@ -23,4 +23,56 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+const processEntries = () => {
+  // get form controls to check for validity
+  const name = $("#name");
+  const passport = $("#passport");
+  const country = $("#country");
+  const terms = $("#terms");
 
+  // check user entries for validity
+  let isValid = true;
+  if (name.value == "") {
+      name.nextElementSibling.textContent = "<span1>";
+      isValid = false; 
+  } else {
+      name.nextElementSibling.textContent = ""; 
+  }
+
+  var numberOfChars = 6;
+  if (passport.value == ""){
+    ;
+
+      passport.nextElementSibling.textContent = "<span1>";
+      isValid = false; 
+  } else { 
+      passport.nextElementSibling.textContent = "";
+  }
+
+  if (creditcard.value == "") {
+      creditcard.nextElementSibling.textContent = "<span3>";
+      isValid = false; 
+  } else { 
+      $("#creditcard").nextElementSibling.textContent = "";
+  }
+
+ 
+  // submit the form if all fields are valid
+  if (isValid == true) {
+      $("form").submit(); 
+  }
+};
+
+const resetForm = () => {
+  $("form").reset();
+  $("#name").nextElementSibling.textContent = "*";
+  $("#passport").nextElementSibling.textContent = "*";
+  $("#creditcard").nextElementSibling.textContent = "*";	
+  
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  $("#process").addEventListener("click", processEntries);
+  $("#reset_form").addEventListener("click", resetForm);  
+    
+});
